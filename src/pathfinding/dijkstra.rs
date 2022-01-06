bfdeaaa71833960661ae91a2b5ad3ed0bab2e2ed
sypan <stephenyuehpan@gmail.com>
2022-01-06 02:36:16 -0800

    removed common fn

--- src/pathfinding/dijkstra.rs
+++ src/pathfinding/dijkstra.rs
@@ -1,28 +1,7 @@
 use crate::datatype::vertex::Vertex;
+use crate::pathfinding::lib::{get_next_loc, is_invalid_loc};
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
-
-const DIRECTION: [(i32, i32); 4] = [
-    (0, -1), // Left
-    (1, 0),  // Up
-    (0, 1),  // Right
-    (-1, 0), // Down
-];
-
-/// Size of tuple is (i32, i32) to avoid the edge case where
-/// units in the Vertex (u16, u16) is between 2^15 and 2^16-1.
-fn get_next_loc(loc: Vertex, action: usize) -> (i32, i32) {
-    let dir = DIRECTION[action];
-    (loc.0 as i32 + dir.0, loc.1 as i32 + dir.1)
-}
-
-fn is_invalid_loc(map: &Vec<Vec<u8>>, next_loc: (i32, i32)) -> bool {
-    let height = map.len() as i32;
-    let width = map[0].len() as i32;
-    next_loc.0 < 0 || next_loc.0 >= height // Out of bound in vertical axis
-        || next_loc.1 < 0 || next_loc.1 >= width // Out of bound in horizontal axis
-        || map[next_loc.0 as usize][next_loc.1 as usize] == 0 // Map obstacle, will never panic
-}
 
 /// Use Dijkstra to build a shortest path from a location to all other locations
 pub fn compute_heuristics(map: &Vec<Vec<u8>>, start_loc: Vertex) -> HashMap<Vertex, u16> {
@@ -35,26 +14,27 @@ pub fn compute_heuristics(map: &Vec<Vec<u8>>, start_loc: Vertex) -> HashMap<Vert
     while open_list.len() > 0 {
         let cur_node = open_list.pop().unwrap();
         for action in 0..4 {
-            let next_loc = get_next_loc(cur_node.loc, action);
+            let next_loc = match get_next_loc(cur_node.loc, action) {
+                Some(vertex) => vertex,
+                None => continue,
+            };
             if is_invalid_loc(&map, next_loc) {
                 continue;
             }
-            let new_node = Node::new(
-                Vertex(next_loc.0 as u16, next_loc.1 as u16),
-                cur_node.g_val + 1,
-            );
-            match closed_list.get(&new_node.loc) {
+            let new_node = Node::new(next_loc, cur_node.g_val + 1);
+            let key = new_node.loc;
+            match closed_list.get(&key) {
                 Some(node) => {
                     // Update existing node if it is a shorter path
-                    if new_node.g_val < node.g_val {
+                    if new_node < *node {
                         // Update key, guard against the key possibly not being set
-                        let val = closed_list.entry(new_node.loc).or_insert(new_node);
+                        let val = closed_list.entry(key).or_insert(new_node);
                         *val = new_node;
                         open_list.push(new_node);
                     }
                 }
                 None => {
-                    closed_list.insert(new_node.loc, new_node);
+                    closed_list.insert(key, new_node);
                     open_list.push(new_node);
                 }
             }
@@ -90,6 +70,9 @@ impl Ord for Node {
 impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
+    }
+    fn lt(&self, other: &Self) -> bool {
+        self.g_val < other.g_val
     }
 }
 
