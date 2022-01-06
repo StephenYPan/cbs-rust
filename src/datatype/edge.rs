d092d88a06c88b0d8a03a57750937637d88df8e8
sypan <stephenyuehpan@gmail.com>
2022-01-06 00:48:08 -0800

    add format

--- src/datatype/edge.rs
+++ src/datatype/edge.rs
@@ -1,4 +1,11 @@
 use crate::datatype::vertex::Vertex;
+use std::fmt;
 
-#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+#[derive(Eq, PartialEq, Hash, Copy, Clone)]
 pub struct Edge(pub Vertex, pub Vertex);
+
+impl fmt::Debug for Edge {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("").field(&self.0).field(&self.1).finish()
+    }
+}
--- src/datatype/vertex.rs
+++ src/datatype/vertex.rs
@@ -1,2 +1,10 @@
-#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+use std::fmt;
+
+#[derive(Eq, PartialEq, Hash, Copy, Clone)]
 pub struct Vertex(pub u16, pub u16);
+
+impl fmt::Debug for Vertex {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("").field(&self.0).field(&self.1).finish()
+    }
+}
