c5e59d866e893ac0941e4e913bb14b80c35d31aa
sypan <stephenyuehpan@gmail.com>
2022-01-06 17:00:13 -0800

    reduce size of various datatypes

--- src/pathfinding/astar.rs
+++ src/pathfinding/astar.rs
@@ -11,18 +11,12 @@ pub fn astar(
 ) -> Option<Vec<Vertex>> {
     let map_size: usize = map.iter().map(Vec::len).sum();
     let mut open_list: BinaryHeap<Node> = BinaryHeap::new();
-    let mut closed_list: HashMap<(Vertex, u16), Node> = HashMap::with_capacity(map_size);
+    let mut closed_list: HashMap<(Vertex, u16), usize> = HashMap::with_capacity(map_size);
     let mut tree = Tree::new();
-    let root_idx = tree.add_node(
-        start_loc,
-        0,
-        h_values.get(&start_loc).unwrap().clone(),
-        0,
-        None,
-    );
+    let root_idx = tree.add_node(start_loc, 0, *h_values.get(&start_loc).unwrap(), 0, None);
     let root = tree.tree[root_idx];
+    closed_list.insert((root.loc, root.timestep), root_idx);
     open_list.push(root);
-    closed_list.insert((root.loc, root.timestep), root);
     while open_list.len() > 0 {
         let cur_node = open_list.pop().unwrap();
         if cur_node.loc == goal_loc {
@@ -37,27 +31,27 @@ pub fn astar(
                 continue;
             }
             // TODO: add constraints
-            let idx = tree.add_node(
+            let new_idx = tree.add_node(
                 next_loc,
                 cur_node.g_val + 1,
-                h_values.get(&next_loc).unwrap().clone(),
+                *h_values.get(&next_loc).unwrap(),
                 cur_node.timestep + 1,
                 Some(cur_node.idx),
             );
-            let new_node = tree.tree[idx];
+            let new_node = tree.tree[new_idx];
             let key = (new_node.loc, new_node.timestep);
             match closed_list.get(&key) {
-                Some(node) => {
+                Some(old_idx) => {
                     // Update existing node if it is a shorter path
-                    if new_node < *node {
+                    if new_node < tree.tree[*old_idx] {
                         // Update key, guard against the key possibly not being set
-                        let val = closed_list.entry(key).or_insert(new_node);
-                        *val = new_node;
-                        open_list.push(new_node);
+                        let val = closed_list.entry(key).or_insert(new_idx);
+                        *val = new_idx;
+                        open_list.push(new_node)
                     }
                 }
                 None => {
-                    closed_list.insert(key, new_node);
+                    closed_list.insert(key, new_idx);
                     open_list.push(new_node);
                 }
             }
@@ -67,14 +61,17 @@ pub fn astar(
 }
 
 /// source: https://dev.to/deciduously/no-more-tears-no-more-knots-arena-allocated-trees-in-rust-44k6
-#[derive(Debug, Default)]
 struct Tree {
     tree: Vec<Node>,
+    parent_node: Vec<Option<usize>>,
 }
 
 impl Tree {
     fn new() -> Tree {
-        Tree { tree: Vec::new() }
+        Tree {
+            tree: Vec::new(),
+            parent_node: Vec::new(),
+        }
     }
 
     fn add_node(
@@ -90,27 +87,28 @@ impl Tree {
             if n.loc == loc && n.timestep == timestep {
                 n.g_val = g_val;
                 n.h_val = h_val;
-                n.parent = parent;
+                self.parent_node[n.idx] = parent;
                 return n.idx;
             }
         }
         // Add new node and return the index
         let idx = self.tree.len();
-        let node = Node::new(idx, loc, g_val, h_val, timestep, parent);
+        let node = Node::new(idx, loc, g_val, h_val, timestep);
         self.tree.push(node);
+        self.parent_node.push(parent);
         idx
     }
 
     fn get_path(&self, idx: usize) -> Vec<Vertex> {
         // Travel backwards from a index to the root
-        let goal_node = self.tree[idx];
         let mut path: Vec<Vertex> = Vec::new();
+        let goal_node = &self.tree[idx];
         path.push(goal_node.loc);
-        let mut optional = goal_node.parent;
+        let mut optional = self.parent_node[idx];
         while let Some(next_idx) = optional {
-            let next_node = self.tree[next_idx];
+            let next_node = &self.tree[next_idx];
             path.push(next_node.loc);
-            optional = next_node.parent;
+            optional = self.parent_node[next_idx];
         }
         path.reverse();
         path
@@ -124,7 +122,6 @@ struct Node {
     g_val: u16,
     h_val: u16,
     timestep: u16,
-    parent: Option<usize>,
 }
 
 impl PartialEq for Node {
@@ -135,9 +132,9 @@ impl PartialEq for Node {
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
-        let f_value = self.g_val + self.h_val;
-        let other_f_value = other.g_val + other.h_val;
-        f_value.cmp(&other_f_value).reverse()
+        let f_val = self.g_val + self.h_val;
+        let other_f_val = other.g_val + other.h_val;
+        f_val.cmp(&other_f_val).reverse()
     }
 }
 
@@ -151,21 +148,13 @@ impl PartialOrd for Node {
 }
 
 impl Node {
-    fn new(
-        idx: usize,
-        loc: Vertex,
-        g_val: u16,
-        h_val: u16,
-        timestep: u16,
-        parent: Option<usize>,
-    ) -> Node {
+    fn new(idx: usize, loc: Vertex, g_val: u16, h_val: u16, timestep: u16) -> Node {
         Node {
             idx: idx,
             loc: loc,
             g_val: g_val,
             h_val: h_val,
             timestep: timestep,
-            parent: parent,
         }
     }
 }
