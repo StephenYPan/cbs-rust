d3983e83a0768b317bc20248377ab1e977f4cb56
sypan <stephenyuehpan@gmail.com>
2022-01-06 20:14:42 -0800

    reduce node size to 8 bytes

--- src/pathfinding/astar.rs
+++ src/pathfinding/astar.rs
@@ -19,17 +19,17 @@ pub fn astar(
     let mut closed_list: HashMap<(Vertex, u16), usize> = HashMap::with_capacity(map_size);
     let mut tree = Tree::new();
 
-    let root_idx = tree.add_node(start_loc, 0, *h_values.get(&start_loc).unwrap(), 0, 0);
+    let root_idx = tree.add_node(start_loc, 0, *h_values.get(&start_loc).unwrap(), 0);
     let root = tree.tree[root_idx];
-    closed_list.insert((root.loc, root.timestep), root_idx);
+    closed_list.insert((root.loc, root.g_val), root_idx);
     open_list.push(root);
 
     while !open_list.is_empty() {
         let cur_node = open_list.pop().unwrap();
+        let cur_key = (cur_node.loc, cur_node.g_val);
         if cur_node.loc == goal_loc {
-            return Some(tree.get_path(cur_node));
+            return Some(tree.get_path(*closed_list.get(&cur_key).unwrap()));
         }
-        let cur_key = (cur_node.loc, cur_node.timestep);
         for action in 0..5 {
             let next_loc = match get_next_loc(cur_node.loc, action) {
                 Some(vertex) => vertex,
@@ -43,11 +43,10 @@ pub fn astar(
                 next_loc,
                 cur_node.g_val + 1,
                 *h_values.get(&next_loc).unwrap(),
-                cur_node.timestep + 1,
                 *closed_list.get(&cur_key).unwrap(),
             );
             let new_node = tree.tree[new_idx];
-            let new_key = (new_node.loc, new_node.timestep);
+            let new_key = (new_node.loc, new_node.g_val);
             match closed_list.get(&new_key) {
                 Some(old_idx) => {
                     // Update existing node if it is a shorter path
@@ -82,49 +81,27 @@ impl Tree {
         }
     }
 
-    fn add_node(
-        &mut self,
-        loc: Vertex,
-        g_val: u16,
-        h_val: u16,
-        timestep: u16,
-        parent: usize,
-    ) -> usize {
+    fn add_node(&mut self, loc: Vertex, g_val: u16, h_val: u16, parent: usize) -> usize {
         // If node exists, then update and return its index
         for (i, n) in self.tree.iter_mut().enumerate() {
-            if n.loc == loc && n.timestep == timestep {
-                n.g_val = g_val;
-                n.h_val = h_val;
+            if n.loc == loc && n.g_val == g_val {
                 self.parent_node[i] = parent;
                 return i;
             }
         }
         // Add new node and return its index
         let idx = self.tree.len();
-        let node = Node::new(loc, g_val, h_val, timestep);
+        let node = Node::new(loc, g_val, h_val);
         self.tree.push(node);
         self.parent_node.push(parent);
         idx
     }
 
-    /// Runtime is O(n + c) where n is the number of nodes in the tree, and c is
-    /// the path length. We have to iterate through the whole tree to find the
-    /// goal node index and then hop from the goal node index to the start node.
-    /// In practice goal node are usually located in the back of the vector, so
-    /// the runtime is smaller than n.
-    fn get_path(&self, goal_node: Node) -> Vec<Vertex> {
-        // Find index of goal node
-        let mut goal_idx: usize = 0;
-        let num_nodes = self.tree.len();
-        for (i, n) in self.tree.iter().rev().enumerate() {
-            if *n == goal_node {
-                goal_idx = num_nodes - i;
-                break;
-            }
-        }
-        // Travel backwards from the goal index to the start node.
+    /// Runtime is O(c) where c is the path length.
+    fn get_path(&self, goal_idx: usize) -> Vec<Vertex> {
+        // Travel backwards from the goal node to the start node.
         let mut path: Vec<Vertex> = Vec::new();
-        path.push(goal_node.loc);
+        path.push(self.tree[goal_idx].loc);
         let mut next_idx = self.parent_node[goal_idx];
         while next_idx != 0 {
             path.push(self.tree[next_idx].loc);
@@ -141,12 +118,11 @@ struct Node {
     loc: Vertex,
     g_val: u16,
     h_val: u16,
-    timestep: u16,
 }
 
 impl PartialEq for Node {
     fn eq(&self, other: &Self) -> bool {
-        self.loc.eq(&other.loc) && self.timestep.eq(&other.timestep)
+        self.loc.eq(&other.loc) && self.g_val.eq(&other.g_val)
     }
 }
 
@@ -169,12 +145,11 @@ impl PartialOrd for Node {
 }
 
 impl Node {
-    fn new(loc: Vertex, g_val: u16, h_val: u16, timestep: u16) -> Node {
+    fn new(loc: Vertex, g_val: u16, h_val: u16) -> Node {
         Node {
             loc: loc,
             g_val: g_val,
             h_val: h_val,
-            timestep: timestep,
         }
     }
 }
