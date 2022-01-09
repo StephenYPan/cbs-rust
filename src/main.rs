6a36ff603b4179aa5295b13075da59ce126c9f27
sypan <stephenyuehpan@gmail.com>
2022-01-09 13:54:58 -0800

    test cbs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-// #[path = "./pathfinding/dijkstra.rs"]
-// mod dijkstra;
 mod datatype;
-mod pathfinding;
+mod multi_agent;
+mod single_agent;
 
-use datatype::vertex;
+use datatype::vertex::Vertex;
+use multi_agent::cbs::cbs;
+#[allow(unused)]
 use std::mem::size_of_val;
+use std::time::Instant;
 
 fn main() {
     // 4 4
@@ -20,18 +22,26 @@ fn main() {
     map[1][3] = 0;
     map[3][0] = 0;
     map[3][1] = 0;
+    let starts: Vec<Vertex> = vec![Vertex(1, 0), Vertex(0, 1)];
+    let goals: Vec<Vertex> = vec![Vertex(2, 3), Vertex(3, 2)];
+
     for r in &map {
         println!("{:?}", r);
     }
-    println!("map size (bytes): {:?}", size_of_val(&map));
-    let start_loc = vertex::Vertex(1, 0);
-    println!("vertex size (bytes): {:?}", size_of_val(&start_loc));
-    let h_values = pathfinding::dijkstra::compute_heuristics(&map, start_loc);
-    // println!("{:?}", start_loc);
-    for (vertex, g_val) in &h_values {
-        println!("{:?} {:?}", vertex, g_val);
+    println!("{:?}", starts);
+    println!("{:?}", goals);
+
+    let now = Instant::now();
+    let paths = cbs(&map, starts, goals);
+    let elapsed_time = now.elapsed();
+    println!("Search time: {:?}", elapsed_time);
+
+    match paths {
+        Some(paths) => {
+            for (i, p) in paths.iter().enumerate() {
+                println!("agent-{}, path: {:?}", i, p);
+            }
+        }
+        None => println!("No solution."),
     }
-    println!("h_values size (bytes): {:?}", size_of_val(&h_values));
-    // println!("h_values len: {:?}", h_values.len());
-    // let goal_loc = vertex::Vertex(2, 3);
 }
