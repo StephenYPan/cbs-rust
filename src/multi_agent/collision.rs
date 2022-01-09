9f187e10a88ad7e84dec5c4a975f71b6168a9d94
sypan <stephenyuehpan@gmail.com>
2022-01-09 11:54:16 -0800

    add constructor

--- src/multi_agent/collision.rs
+++ src/multi_agent/collision.rs
@@ -1,4 +1,4 @@
-use crate::datatype::{constraint::Location, edge::Edge};
+use crate::datatype::{constraint::Location, edge::Edge, vertex::Vertex};
 use std::fmt;
 
 #[derive(Eq)]
@@ -13,6 +13,22 @@ pub struct Collision {
 impl PartialEq for Collision {
     fn eq(&self, other: &Self) -> bool {
         self.loc.eq(&other.loc) && self.is_edge & other.is_edge && self.timestep == other.timestep
+    }
+}
+
+impl Collision {
+    pub fn new(a1: u8, a2: u8, loc: Location, timestep: u16) -> Collision {
+        let location = match loc {
+            Location::Vertex(v) => (Edge(Vertex(0, 0), v), false),
+            Location::Edge(e) => (e, true),
+        };
+        Collision {
+            a1: a1,
+            a2: a2,
+            loc: location.0,
+            is_edge: location.1,
+            timestep: timestep,
+        }
     }
 }
 
