5bd4537eaca425619f768f585f0c2efa90402c35
sypan <stephenyuehpan@gmail.com>
2022-01-09 13:28:32 -0800

    reduced unnecessary struct var

--- src/multi_agent/collision.rs
+++ src/multi_agent/collision.rs
@@ -1,32 +1,26 @@
-use crate::datatype::{constraint::Location, edge::Edge, vertex::Vertex};
+use crate::datatype::constraint::Location;
 use std::fmt;
 
 #[derive(Eq)]
 pub struct Collision {
     pub a1: u8,
     pub a2: u8,
-    pub loc: Edge,
-    pub is_edge: bool,
+    pub loc: Location,
     pub timestep: u16,
 }
 
 impl PartialEq for Collision {
     fn eq(&self, other: &Self) -> bool {
-        self.loc.eq(&other.loc) && self.is_edge & other.is_edge && self.timestep == other.timestep
+        self.timestep == other.timestep && self.loc.eq(&other.loc)
     }
 }
 
 impl Collision {
     pub fn new(a1: u8, a2: u8, loc: Location, timestep: u16) -> Collision {
-        let location = match loc {
-            Location::Vertex(v) => (Edge(Vertex(0, 0), v), false),
-            Location::Edge(e) => (e, true),
-        };
         Collision {
             a1: a1,
             a2: a2,
-            loc: location.0,
-            is_edge: location.1,
+            loc: loc,
             timestep: timestep,
         }
     }
@@ -34,15 +28,10 @@ impl Collision {
 
 impl fmt::Debug for Collision {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let loc: Location = if self.is_edge {
-            Location::new(self.loc)
-        } else {
-            Location::new(self.loc.1)
-        };
         f.debug_struct("Collision")
             .field("a1", &self.a1)
             .field("a2", &self.a2)
-            .field("loc", &loc)
+            .field("loc", &self.loc)
             .field("timestep", &self.timestep)
             .finish()
     }
